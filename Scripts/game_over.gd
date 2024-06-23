579052b4671efae5f2fc7d0ab9c6ad5b706b6fdf
gobbe <megobbt@gmail.com>
2024-06-23 22:44:55 +0200

    Gameoverscreen

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -1,8 +1,14 @@
 extends Control
+@onready var bobAnim = $AnimatedSprite2D
+@export var sadThresh:int
 
 func ready():
 	Global.clicking_enabled = true
-	
+	if(Global.money < sadThresh):
+		bobAnim.play("weep")
+	else:
+		bobAnim.play("idle")
+
 func _process(_delta):
 	Engine.time_scale = 1
 	$MoneyLabel.text = "$" + str(Global.money)
